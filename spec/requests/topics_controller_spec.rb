8372883b6fe4a66cda9c65b7d81148194642d462
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2021-08-02 20:00:41 +0200

    Specs for topic#update

--- spec/requests/topics_controller_spec.rb
+++ spec/requests/topics_controller_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe TopicsController do
   fab!(:user) { Fabricate(:user) }
   fab!(:moderator) { Fabricate(:moderator) }
   fab!(:admin) { Fabricate(:admin) }
+  fab!(:trust_level_0) { Fabricate(:trust_level_0) }
+  fab!(:trust_level_1) { Fabricate(:trust_level_1) }
   fab!(:trust_level_4) { Fabricate(:trust_level_4) }
 
   fab!(:category) { Fabricate(:category) }
@@ -1648,6 +1650,28 @@ RSpec.describe TopicsController do
             expect(topic.reload.category).to eq(category)
           end
         end
+      end
+    end
+
+    describe "urls in the title" do
+      let!(:title_with_url) { "A title with the URL https://google.com" }
+
+      it "doesn't allow TL0 users to put urls into the title" do
+        sign_in(trust_level_0)
+        topic = Fabricate(:topic, user: trust_level_0)
+        Fabricate(:post, topic: topic)
+        put "/t/#{topic.slug}/#{topic.id}.json", params: { title: title_with_url }
+
+        expect(response.status).to eq(403)
+      end
+
+      it "allows TL1 users to put urls into the title" do
+        sign_in(trust_level_1)
+        topic = Fabricate(:topic, user: trust_level_1)
+        Fabricate(:post, topic: topic)
+        put "/t/#{topic.slug}/#{topic.id}.json", params: { title: title_with_url }
+
+        expect(response.status).to eq(200)
       end
     end
   end
